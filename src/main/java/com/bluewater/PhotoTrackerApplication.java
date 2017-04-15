24f7b3a2c24b1a7a0c5e275c486e4e39dd9e57da
Jeff C <bluewater_dev123@yahoo.com>
2017-04-15 08:54:03 -0700

    first commit

---
+++ src/main/java/com/bluewater/PhotoTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.bluewater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bluewater/PhotoTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bluewater;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
