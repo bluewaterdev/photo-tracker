20437764e84777ce092006128573a43a9bd85360
Jeff C <bluewater_dev123@yahoo.com>
2017-04-20 15:40:36 -0700

    hello example with login and logout page, and CSRF security

--- src/main/java/com/bluewater/PhotoTrackerApplication.java
+++ src/main/java/com/bluewater/PhotoTrackerApplication.java
@@ -1,11 +1,70 @@
 package com.bluewater;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PhotoTrackerApplication {
 
+	// used by authenticate() js method ( naming conventions )
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World REST");
+		return model;
+	}
+
+
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.httpBasic().and().logout().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.csrf() // below adds filter that passes cookie with CSRF token to prevent Cross site forgery attacks ( CSRF )
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
+	}
+	
+	// tell Spring Security to expect the CSRF token in the format that Angular
+	// wants to send it back (a header called “X-XRSF-TOKEN” instead of the
+	// default “X-CSRF-TOKEN”). 
+	private CsrfTokenRepository csrfTokenRepository() {
+		  HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		  repository.setHeaderName("X-XSRF-TOKEN");
+		  return repository;
+		}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoTrackerApplication.class, args);
 	}
